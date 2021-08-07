8b660cbc4985470d7bedb4c8c160170320469503
윤정인 <yoonjeongin@naver.com>
2021-08-07 17:56:16 +0900

    add padStart function

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,10 @@ const clock = document.querySelector("#clock");
 
 function getClock(){
     const date = new Date();
-    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock(); //website가 load 되자마자 실행시켜주는 용 (없으면 처음 들어 갔을 때 00:00:00으로 시작)
