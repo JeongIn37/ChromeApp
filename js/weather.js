c139614d4b6aa58fb9245f72343fc55acaba2407
윤정인 <yoonjeongin@naver.com>
2021-08-22 16:39:34 +0900

    show weather

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,20 @@
+const API_KEY = "439c166fb6f21d96096bd4b20c8f7270"
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude; 
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude; 
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metirc`
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = Math.round(parseInt(data.main.temp)) + "℃  " + data.weather[0].main;
+        });
 }
 
-function onGeoError(){
+function onGeoError(){ 
     alert("Can't find you. No weather for you.");
 }
 
